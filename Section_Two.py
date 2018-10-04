8a162802e09c4f828efa7f5f56dded36753e9c41
cristinast <gg.cristinast@gmail.com>
2018-10-04 10:30:51 +0900

    11 finish
    emmm finish.

--- Section_Two.py
+++ Section_Two.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
@@ -6,11 +6,31 @@
 
 
 #count 10 行数のカウント
-import sys
-import os
-import re
 
-data = sys.stdin.read('hightemp.txt')
-lines = data.count('\n')
+#just use to count small file
+#count = len(open('hightemp.txt','rU').readlines()) 
 
-print "%(lines)s" %locals()
+# for big file,use circulation to count numbers of line
+'''
+count  = -1
+for count,line in enumerate(open(r'hightemp.txt','rU')):
+    pass
+count += 1 
+'''
+
+#the best way,use carriage return to count numbers of line.
+'''
+count = 0
+fp = open('hightemp.txt') #open files
+while True:
+    buffer = fp.read(1024*8192)
+    if not buffer:
+        break
+    count += buffer.count('\n')
+fp.close()
+'''
+
+#print (count)
+
+
+#replace blank 11 タブをスペースに置換
