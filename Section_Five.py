35e6de53e7910297a19f70404846cc582e420444
cristinast <gg.cristinast@gmail.com>
2019-05-15 15:25:13 +0900

    start 40

--- Section_Five.py
+++ Section_Five.py
@@ -6,3 +6,45 @@
 import CaboCha
 import sys
 
+
+class Morpheme():
+    def __init__(self,surface,base,pos,pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+    
+    def Output_data(self):
+        print('surface[{}]\tbase[{}]\tpos[{}]\tpos1[{}]'.format(self.surface,self.base,self.pos,self.pos1))
+
+
+
+def Parse_file():
+    with open('neko.txt','r') as fp:
+        with open('neko.txt.cabocha','w') as fp1:
+            cabocha = CaboCha.Parser()
+            for line in fp:
+                fp1.write(cabocha.parse(line).toString(CaboCha.FORMAT_LATTICE))
+
+
+Parse_file()
+sentence = []
+sentences = []
+
+with open('neko.txt.cabocha','r') as nekocabocha:
+    for line in nekocabocha:
+        if line == 'EOS\n':
+            sentences.append(sentence)
+            sentence = []
+        elif line[0] == '*':
+            continue
+        else:
+            surface,other = line.split()
+            others = other[1].split(',')
+            base = others[6]
+            pos = others[0]
+            pos1 = others[1]
+            morpheme = Morpheme(surface,base,pos,pos1)
+            sentence.append(morpheme)
+
+for m in sentencs
